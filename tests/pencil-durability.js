9b4a950e703bee9593d31120e1881fcc84da494a
Christopher <cruss1989@gmail.com>
2018-03-29 01:14:32 -0400

    mistakes, test never failed. passing test: paper concatonates text

--- tests/pencil-durability.js
+++ tests/pencil-durability.js
@@ -12,6 +12,16 @@ var PencilDurabilityTests = function(){
 		return actual == expected
 	}
 
+	this['When I add more text to the paper, the paper reflects the entire string.'] = function(){
+		var pencil = new Pencil
+		var paper = new Paper
+		pencil.write(paper,'Writing is a good way')
+		pencil.write(paper,' to remember things.')
+		var expected = 'Writing is a good way to remember things.'
+		var actual = paper.text
+		return actual == expected
+	}
+
 }
 
 module.exports = PencilDurabilityTests
